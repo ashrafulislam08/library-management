a1cf1dc3d37ce5e18627d7bfeceb993f0a73b708
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-06-22 12:16:56 +0600

    borrow interface and schema design completed

---
+++ src/app/models/borrow.model.ts
@@ -0,0 +1,19 @@
+import { model, Schema } from "mongoose";
+import { IBorrow } from "../../interfaces/borrow.interface";
+
+const borrowSchema = new Schema<IBorrow>({
+  book: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export const Borrow = model("Borrow", borrowSchema);
